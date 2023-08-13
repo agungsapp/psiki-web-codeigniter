62d4175577a330495f54285dd34b81fd85c49a46
agungsapp <agung.dni19@gmail.com>
2023-08-13 15:59:45 +0700

    update 13 agustus

--- app/Views/admin/layouts/partials/sidebar.php
+++ app/Views/admin/layouts/partials/sidebar.php
@@ -4,7 +4,7 @@
         <!-- Sidebar navigation-->
         <nav class="sidebar-nav">
           <ul id="sidebarnav" class="pt-4">
-            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="index.html" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</span></a></li>
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/admin/dashboard" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</span></a></li>
             <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/admin/saran" aria-expanded="false"><i class="mdi mdi-book-multiple"></i><span class="hide-menu">Data Saran</span></a></li>
             <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="widgets.html" aria-expanded="false"><i class="mdi mdi-library-books"></i><span class="hide-menu">Jenis Gangguan</span></a></li>
             <!-- <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="tables.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">Data Users</span></a></li>
